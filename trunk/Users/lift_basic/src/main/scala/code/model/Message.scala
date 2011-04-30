7e1dd1cfbc2b8574ff8859104a1bd98369db04cf
mattia.candeloro89 <mattia.candeloro89@414e8b3e-6ff4-ec89-4245-23444c5df0c2>
2011-04-30 13:28:28 +0000

    Mattia, Wahed, Andrea: working on messages

--- trunk/Users/lift_basic/src/main/scala/code/model/Message.scala
+++ trunk/Users/lift_basic/src/main/scala/code/model/Message.scala
@@ -1,38 +1,31 @@
 package code
 package model
 
-
 import net.liftweb.mapper._
 import net.liftweb.util._
 import net.liftweb.common._
-import java.math.MathContext
 
+import java.math.MathContext
 
 object Message extends Message with LongKeyedMetaMapper[Message] {
 
-	
 }
-
 
 class Message extends LongKeyedMapper[Message] with IdPK {
 	
 	def getSingleton = Message
+
+	object userID extends MappedLong(this)
 	
-	object profile extends MappedString(this, 30) 
-	
-	
-	object userID extends MappedDecimal(this, MathContext.DECIMAL64, 0)
-	
-	object messageNumber extends MappedString(this, 30)
-	
-	object from extends MappedString(this, 30)
+	object messageNumber extends MappedDecimal(this, MathContext.DECIMAL64, 0)
+
+	object fromID extends MappedString(this, 50)
 	
 	object date extends MappedDateTime(this)
 	
-	object text extends MappedString(this, 150)
-	
-	
-	
-	
-					
-}	
+	 object text extends MappedTextarea(this, 2048) {
+    override def textareaRows  = 10
+    override def textareaCols = 50
+    override def displayName = "Message"
+  }
+}
--- trunk/Users/lift_basic/src/main/scala/code/model/User.scala
+++ trunk/Users/lift_basic/src/main/scala/code/model/User.scala
@@ -4,6 +4,16 @@ package model
 import net.liftweb.mapper._
 import net.liftweb.util._
 import net.liftweb.common._
+import net.liftweb._
+
+import util._
+import Helpers._
+
+import common._
+import http._
+import sitemap._
+import Loc._
+import mapper._
 
 /**
  * The singleton that has methods for accessing the database
@@ -13,8 +23,10 @@ object User extends User with MetaMegaProtoUser[User] {
   override def screenWrap = Full(<lift:surround with="default" at="content">
 			       <lift:bind /></lift:surround>)
   // define the order fields will appear in forms and output
-  override def fieldOrder = List(id, firstName, lastName, email,
-   password, textArea)
+  override def fieldOrder = List(id, accountID, firstName, lastName, email,
+  locale, timezone, password, skypeID, gender, textArea)
+
+  override def signupFields = List(accountID, firstName, lastName, password, email, gender, skypeID)	
 
   // comment this line out to require email validations
   override def skipEmailValidation = true
@@ -24,7 +36,17 @@ object User extends User with MetaMegaProtoUser[User] {
  * An O-R mapped "User" class that includes first name, last name, password and we add a "Personal Essay" to it
  */
 class User extends MegaProtoUser[User] {
-  def getSingleton = User // what's the "meta" server
+  	def getSingleton = User // what's the "meta" server
+  	
+	object accountID extends MappedString(this, 30){
+		override def validations = valUnique(S.?("user.unique.accountID")) _ :: super.validations
+	}
+
+	object skypeID extends MappedString(this, 30) {
+	}
+	
+	object gender extends MappedGender(this){
+	}
 
   // define an additional field for a personal essay
   object textArea extends MappedTextarea(this, 2048) {
@@ -33,4 +55,3 @@ class User extends MegaProtoUser[User] {
     override def displayName = "Personal Essay"
   }
 }
-
