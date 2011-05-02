d7da11e7ccd9d70f1e8c9322e189f73448b8a53a
yotamsharon <yotamsharon@414e8b3e-6ff4-ec89-4245-23444c5df0c2>
2011-05-02 13:19:27 +0000

    thomas and yotam added chat prototype

---
+++ trunk/chat/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -0,0 +1,117 @@
+package bootstrap.liftweb
+
+import net.liftweb._
+import util._
+import Helpers._
+
+import common._
+import http._
+import sitemap._
+import Loc._
+import mapper._
+
+import js._
+import JsCmds._
+import JE._
+
+import scala.xml.NodeSeq
+
+import code.model._
+import code.comet.ChatServer
+import scala.collection.immutable._
+
+
+/**
+ * A class that's instantiated early and run.  It allows the application
+ * to modify lift's environment
+ */
+class Boot {
+  def boot {
+    if (!DB.jndiJdbcConnAvailable_?) {
+      val vendor = 
+	new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
+			     Props.get("db.url") openOr 
+			     "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
+			     Props.get("db.user"), Props.get("db.password"))
+
+      LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
+
+      DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
+    }
+
+    // Use Lift's Mapper ORM to populate the database
+    // you don't need to use Mapper to use Lift... use
+    // any ORM you want
+    Schemifier.schemify(true, Schemifier.infoF _, User)
+
+    // where to search snippet
+    LiftRules.addToPackages("code")
+
+    // Build SiteMap
+    def sitemap = SiteMap(
+      Menu.i("Home") / "index" >> User.AddUserMenusAfter, // the simple way to declare a menu
+
+      // more complex because this menu allows anything in the
+      // /static path to be visible
+      Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
+	       "Static Content")),
+	  Menu(Loc("Chat", Link(List("chat"), true, "/chat/"), 
+	       "Chat Content"))     )
+
+    def sitemapMutators = User.sitemapMutator
+
+    // set the sitemap.  Note if you don't want access control for
+    // each page, just comment this line out.
+    LiftRules.setSiteMapFunc(() => sitemapMutators(sitemap))
+
+    //Show the spinny image when an Ajax call starts
+    LiftRules.ajaxStart =
+      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+    
+    // Make the spinny image go away when it ends
+    LiftRules.ajaxEnd =
+      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
+
+    // Force the request to be UTF-8
+    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
+
+	LiftRules.viewDispatch.append {
+    	case "chat" :: Nil => Right(ChatView)
+
+    }
+
+    // What is the function to test if a user is logged in?
+    LiftRules.loggedInTest = Full(() => User.loggedIn_?)
+
+    // Make a transaction span the whole HTTP request
+    S.addAround(DB.buildLoanWrapper)
+  }
+  
+  object ChatView extends LiftView {
+  	def dispatch = {
+  		case filename:String => () => Full(content(filename))	
+  	}
+  	
+  	def content(filename:String):NodeSeq = {
+
+  	 <lift:surround with="default" at="content">
+  	 
+      <lift:comet type="Chat" name={filename}>
+        <h5 style="text-align:center">Chat</h5>
+        <ul>
+          <li>A message</li>
+        </ul>
+      </lift:comet>
+      
+	  <div>
+        <form class="lift:ChatIn?form=post">
+          <input id="chat_in" />
+	      <input type="hidden" name="filename" value={filename}/>
+	      <input type="hidden" name="name" value="Jack"/>
+          <input type="submit" value="Send"/>
+        </form>
+      </div>
+      </lift:surround>
+  	}	
+  }
+}
---
+++ trunk/chat/src/main/scala/code/comet/ClientsContainer.scala
@@ -0,0 +1,29 @@
+package code
+package comet
+
+import net.liftweb._
+import http._
+import actor._
+import scala.actors._
+import js._
+import JsCmds._
+import JE._
+
+
+case class SubscribeClient(client:Chat)
+case class UnsubClient(client:Chat)
+case object Message
+ 
+object ClientsContainer extends Actor {
+  
+  	private var clients: List[Chat] = List()
+  
+  	def act = loop {		react { 
+			case SubscribeClient(client) => clients ::= client
+			case UnsubClient(client) => clients.filterNot(_ == client)
+			case Message => SetHtml("test", <b>Got message</b>); clients.foreach(_ ! Message)		}	}
+	
+	def getClients():List[Chat] = {
+		return clients	
+	}
+}
