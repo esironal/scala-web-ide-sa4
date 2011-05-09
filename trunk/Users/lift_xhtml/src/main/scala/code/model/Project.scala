cacb3778eb5467db7c4ec061e2a71dd3e37bb491
igormoreno <igormoreno@414e8b3e-6ff4-ec89-4245-23444c5df0c2>
2011-05-09 22:49:05 +0000

    First draft of the integration between file manager and user profile

--- trunk/Users/lift_xhtml/src/main/scala/code/model/Project.scala
+++ trunk/Users/lift_xhtml/src/main/scala/code/model/Project.scala
@@ -2,24 +2,27 @@ package code
 package model
 
 import net.liftweb.mapper._
-import net.liftweb.util._
-import net.liftweb.common._
-
-import java.math.MathContext
 
 object Project extends Project with LongKeyedMetaMapper[Project] {
-	override def dbTableName = "projects"
-	
-	override def fieldOrder = List(id, projectPath)
+    override def dbTableName = "projects"
+
+    override def fieldOrder = List(id, name)
+    
+    val basePath = "/tmp/"
+
+    def getProjectByIdAndByCurrentUser(projectId: String) = {
+    	val currentUser = User.currentUser.open_! 
+        currentUser.projects.find(_.id.is == projectId.toLong).get
+    }
+
 }
 
-class Project extends LongKeyedMapper[Project] with IdPK {
-	
-	def getSingleton = Project
+class Project extends LongKeyedMapper[Project] with IdPK with ManyToMany {
+    def getSingleton = Project
 
-	object projectPath extends MappedString(this, 150){
-		override def displayName = "Project Path"
-	}
-	
+    def path = Project.basePath + this.id.toString
 
+    object name extends MappedString(this, 255)
+
+    object users extends MappedManyToMany(UserProject, UserProject.project, UserProject.user, User)
 }
