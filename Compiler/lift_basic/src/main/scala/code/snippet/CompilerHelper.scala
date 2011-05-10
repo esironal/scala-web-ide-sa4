fb5f0db3454ede24e8ef08bc9111db7b2f9d2b56
baracchi.lorenzo <baracchi.lorenzo@gmail.com>
2011-05-10 00:07:48 +0000

    Added a sort of makefile and some other useful things

--- Compiler/lift_basic/src/main/scala/code/snippet/CompilerHelper.scala
+++ Compiler/lift_basic/src/main/scala/code/snippet/CompilerHelper.scala
@@ -8,7 +8,7 @@ import java.io._
  * Classed used to provide well formatted paramenteres to the Compiler
  * @autor: Lorenzo Baracchi <lorenzo.baracchi@member.fsf.org>	
  */
-class CompilerHelper (id: Int, optionList: scala.Array[String]) {
+class CompilerHelper (id: Int, var optionList: scala.Array[String]) {
 
 	val runTime = Runtime.getRuntime 
 	val projectId = "project"+id
@@ -28,12 +28,32 @@ class CompilerHelper (id: Int, optionList: scala.Array[String]) {
 	 {
 	 	projectId+"/log"
 	 }
+	 
+	 /*
+	  * @return the list of options	
+	  */
+	 def getOptionList = optionList 
+	 
+	 /*
+	  * Set (as compiler option) the folder where to put bins
+	  */	
+	 private def setBinDir() {
+	 	optionList=optionList++scala.Array("-d "+projectId+"/bin")
+	 }
+	 /*
+	  * Set (as compiler option) the folder where to put source files
+	  */	
+	 private def setSrcDir() {
+	 	optionList=optionList++scala.Array("-d "+projectId+"/src")
+	 }
 	
 	/**
 	 * Create the directories need to compile the project
 	 * @return true if the process suceeded, false otherwise
 	 */
 	def createCompilerBox() = {
+		setBinDir()
+		setSrcDir()
 		// create a command to do that!
 		var cmd = "mkdir -p "+projectId+" "+projectId+"/log"+" "+projectId+"/src"+" "+projectId+"/bin"
 		val command= scala.Array("/bin/bash", "-c", cmd)
@@ -51,6 +71,21 @@ class CompilerHelper (id: Int, optionList: scala.Array[String]) {
   		else
   			false
 	}
+	
+	/**
+	 * Create a sort of makefile	
+	 */
+	 def makeMakefile() = {
+	 	var cmd="ls -1R --file-type " + getPath + "&> " + projectId + "/.makefile"
+	 	val c = scala.Array("/bin/bash", "-c", cmd)
+  		val exec = runTime.exec(c)
+  		
+		// wait for the end of the command execution
+  		exec.waitFor()
+  		
+		//return the exit value   
+  		exec.exitValue()
+	 }
 	
 	/**
 	 * Destroy the directories used to compile the project
@@ -84,12 +119,14 @@ class CompilerHelper (id: Int, optionList: scala.Array[String]) {
   		var ch = new CompilerHelper(1, scala.Array("-verbose"));
   		var o=ch.createCompilerBox();
   		println("folders created: "+o)
-  		var t0=System.currentTimeMillis();
+  		var 0=ch.makeMakefile()
+  		println("makefile: "+o)
+  	/*	var t0=System.currentTimeMillis();
   		var t1=t0
           do{
               t1=System.currentTimeMillis();
           }while (t1-t0<5000);
 		o=ch.destroyCompilerBox();
-  		println("folders deleted: "+o)
+  		println("folders deleted: "+o) */
   	}
 }
