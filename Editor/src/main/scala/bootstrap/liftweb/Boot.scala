77c7f720546854a4b75c13000a861c34815ad1f9
mattia.candeloro89 <mattia.candeloro89@414e8b3e-6ff4-ec89-4245-23444c5df0c2>
2011-05-10 14:49:23 +0000

    new merged Boot.scala

--- Editor/src/main/scala/bootstrap/liftweb/Boot.scala
+++ Editor/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -20,6 +20,13 @@ import code.model._
 import code.comet.ChatServer
 import scala.collection.immutable._
 
+import code.model.User
+import code.model.ProfileMessage
+import code.model.Project
+import code.model.UserProject
+import code.model.FileChatMessage
+import code.model.ProjectChatMessage
+
 
 /**
  * A class that's instantiated early and run.  It allows the application
@@ -42,8 +49,8 @@ class Boot {
     // Use Lift's Mapper ORM to populate the database
     // you don't need to use Mapper to use Lift... use
     // any ORM you want
-    Schemifier.schemify(true, Schemifier.infoF _, User)
-
+    Schemifier.schemify(true, Schemifier.infoF _, User, ProfileMessage, Project, UserProject, ProjectChatMessage, FileChatMessage)
+    
     // where to search snippet
     LiftRules.addToPackages("code")
 
@@ -55,6 +62,18 @@ class Boot {
       // /static path to be visible
       Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
 	       "Static Content")),
+	      
+     Menu(Loc("Profile",
+              Link(List("profile"), true, "/profile/"), "Profile")),
+     Menu(Loc("Stats",
+              Link(List("stats"), true, "/stats/"), "Stats")),
+     Menu(Loc("UserList",
+              Link(List("userList"), true, "/userList/"), "UserList")),
+     Menu(Loc("Delete",
+              Link(List("delete"), true, "/delete/"), "Delete")),
+     Menu(Loc("Project", Link(List("project"), true, "/project"), "Project")),
+     Menu(Loc("ProjectList",
+              Link(List("projectList"), true, "/projectList"), "ProjectList")),
 	  Menu(Loc("Chat", Link(List("chat"), true, "/chat/"), 
 	       "Chat Content"))     )
 
@@ -76,7 +95,16 @@ class Boot {
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
     
     LiftRules.viewDispatch.append {
-    	case "chat" :: Nil => Right(ChatView)
+    	case "chat" :: Nil => 
+    		Right(Chat)
+    	case "profile"::Nil =>
+       		Right(Profile)
+     	case "stats"::Nil =>
+     		Right(Stats)
+     	case "delete" :: Nil =>
+     		Right(Delete)
+     	case "userList" :: Nil =>
+     		Right(UserList)
     }
 
     // What is the function to test if a user is logged in?
@@ -84,108 +112,5 @@ class Boot {
 
     // Make a transaction span the whole HTTP request
     S.addAround(DB.buildLoanWrapper)
-  }
-  
-  object ChatView extends LiftView {
-  	def dispatch = {
-  		case filename:String => () => Full(content(filename))	
-  	}
-  	
-  	def content(filename:String):NodeSeq = {	
-  	 <lift:surround with="default" at="content">
-		<!--<hr>-->
-		<table id="main_table">
-			<tr>
-				<td id="left_sidebar" value="true">
-				FILE BROWSER
-				</td>			
-				<td id="codearea">
-					<textarea id={filename} name={filename} class="editor"></textarea>
-					<div id="log">
-						<div class='logDiv'>
-
-						<table class='logTable' cellspacing='0'>
-
-						<tr class='logTrErrorFirst'>
-
-						<td class='logTd1'>17</td><td class='logTd2'><a href='http://NetBeansProjects/SA4Game/src/sa4game/Client.scala#17'>NetBeansProjects/SA4Game/src/sa4game/Client.scala:17</a></td>
-
-						</tr>
-
-						<tr class='logTrErrorSecond'>
-
-						<td class='logTd1'></td><td class='logTd2'>Error - not found: type HashMap</td>
-
-						</tr>
-
-						<tr class='logTrErrorSecond'>
-
-						<td class='logTd1'></td><td class='logTd2 logTdFixed'>  var games = new <u>HashMap</u>[String,Array[Array[Array[Character]]]]</td>
-
-						</tr>
-
-						<tr class='logTrErrorFirst'>
-
-						<td class='logTd1'>12345</td><td class='logTd2'><a href='http://NetBeansProjects/SA4Game/src/sa4game/Client.scala#12345'>NetBeansProjects/SA4Game/src/sa4game/Client.scala:12345</a></td>
-
-						</tr>
-
-						<tr class='logTrErrorSecond'>
-
-						<td class='logTd1'></td><td class='logTd2'>Error - not found: type Random</td>
-
-						</tr>
-
-						<tr class='logTrErrorSecond'>
-
-						<td class='logTd1'></td><td class='logTd2 logTdFixed'>    val rnd = new <u>Random</u></td>
-
-						</tr>
-
-						<tr class='logTrFinalError'>
-
-						<td class='logTd1'></td><td class='logTd2'>2 errors found</td>
-
-						</tr>
-
-						<tr class='logTrFinalCompileFailure'>
-
-						<td class='logTd1'></td><td class='logTd2'>COMPILATION FAILED</td>
-
-						</tr>
-
-						</table>
-
-						</div>
-						
-					</div>
-				</td>
-					<td id="right_sidebar" value="true">
-					<div id="chat_content">
-					<lift:comet type="Chat" name={filename}>
-				        <h5 style="text-align:center">Chat</h5>
-				        <ul>
-				          <li>A message</li>
-				        </ul>
-				     </lift:comet>
-				      
-					  <div>
-				        <form class="lift:ChatIn?form=post">
-				          <input id="chat_in" />
-					      <input type="hidden" name="filename" value={filename}/>
-					      <input type="hidden" name="name" value="Jack"/>
-				          <input type="submit" value="Send"/>
-				        </form>
-				      </div>
-					</div>
-					</td>
-				</tr>
-			</table>
-			<div id="box" class="dialog">
-			<div style="text-align:center"><span id="txt">A nice file viewer</span><br />
-			<button onclick="hm('box');okSelected()">OK</button></div>
-			</div>
-      </lift:surround>
-  	}	
   }
 }
