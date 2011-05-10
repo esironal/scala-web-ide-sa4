4ce3fcc309a14f809221b744e708800dcb0c36be
mattia.candeloro89 <mattia.candeloro89@414e8b3e-6ff4-ec89-4245-23444c5df0c2>
2011-05-10 14:52:05 +0000

    merging Editor with Users

---
+++ Editor/src/main/scala/bootstrap/liftweb/Chat.scala
@@ -0,0 +1,124 @@
+package bootstrap.liftweb
+
+import net.liftweb._
+import util._
+import Helpers._
+
+import common._
+import http._
+import sitemap._
+import Loc._
+import mapper._
+
+import js._
+import JsCmds._
+import JE._
+
+import scala.xml.NodeSeq
+
+import code.model._
+import code.comet.ChatServer
+import scala.collection.immutable._
+
+object Chat extends LiftView {
+  	def dispatch = {
+  		case filename:String => () => Full(content(filename))	
+  	}
+  	
+  	def content(filename:String):NodeSeq = {	
+  	 <lift:surround with="default" at="content">
+		<!--<hr>-->
+		<table id="main_table">
+			<tr>
+				<td id="left_sidebar" value="true">
+				FILE BROWSER
+				</td>			
+				<td id="codearea">
+					<textarea id={filename} name={filename} class="editor"></textarea>
+					<div id="log">
+						<div class='logDiv'>
+
+						<table class='logTable' cellspacing='0'>
+
+						<tr class='logTrErrorFirst'>
+
+						<td class='logTd1'>17</td><td class='logTd2'><a href='http://NetBeansProjects/SA4Game/src/sa4game/Client.scala#17'>NetBeansProjects/SA4Game/src/sa4game/Client.scala:17</a></td>
+
+						</tr>
+
+						<tr class='logTrErrorSecond'>
+
+						<td class='logTd1'></td><td class='logTd2'>Error - not found: type HashMap</td>
+
+						</tr>
+
+						<tr class='logTrErrorSecond'>
+
+						<td class='logTd1'></td><td class='logTd2 logTdFixed'>  var games = new <u>HashMap</u>[String,Array[Array[Array[Character]]]]</td>
+
+						</tr>
+
+						<tr class='logTrErrorFirst'>
+
+						<td class='logTd1'>12345</td><td class='logTd2'><a href='http://NetBeansProjects/SA4Game/src/sa4game/Client.scala#12345'>NetBeansProjects/SA4Game/src/sa4game/Client.scala:12345</a></td>
+
+						</tr>
+
+						<tr class='logTrErrorSecond'>
+
+						<td class='logTd1'></td><td class='logTd2'>Error - not found: type Random</td>
+
+						</tr>
+
+						<tr class='logTrErrorSecond'>
+
+						<td class='logTd1'></td><td class='logTd2 logTdFixed'>    val rnd = new <u>Random</u></td>
+
+						</tr>
+
+						<tr class='logTrFinalError'>
+
+						<td class='logTd1'></td><td class='logTd2'>2 errors found</td>
+
+						</tr>
+
+						<tr class='logTrFinalCompileFailure'>
+
+						<td class='logTd1'></td><td class='logTd2'>COMPILATION FAILED</td>
+
+						</tr>
+
+						</table>
+
+						</div>
+						
+					</div>
+				</td>
+					<td id="right_sidebar" value="true">
+					<div id="chat_content">
+					<lift:comet type="Chat" name={filename}>
+				        <h5 style="text-align:center">Chat</h5>
+				        <ul>
+				          <li>A message</li>
+				        </ul>
+				     </lift:comet>
+				      
+					  <div>
+				        <form class="lift:ChatIn?form=post">
+				          <input id="chat_in" />
+					      <input type="hidden" name="filename" value={filename}/>
+					      <input type="hidden" name="name" value="Jack"/>
+				          <input type="submit" value="Send"/>
+				        </form>
+				      </div>
+					</div>
+					</td>
+				</tr>
+			</table>
+			<div id="box" class="dialog">
+			<div style="text-align:center"><span id="txt">A nice file viewer</span><br />
+			<button onclick="hm('box');okSelected()">OK</button></div>
+			</div>
+      </lift:surround>
+  	}	
+}
--- Editor/src/main/scala/code/model/User.scala
+++ Editor/src/main/scala/code/model/User.scala
@@ -1,9 +1,19 @@
-package code {
-package model {
+package code
+package model
 
-import _root_.net.liftweb.mapper._
-import _root_.net.liftweb.util._
-import _root_.net.liftweb.common._
+import net.liftweb.mapper._
+import net.liftweb.util._
+import net.liftweb.common._
+import net.liftweb._
+
+import util._
+import Helpers._
+
+import common._
+import http._
+import sitemap._
+import Loc._
+import mapper._
 
 /**
  * The singleton that has methods for accessing the database
@@ -12,27 +22,44 @@ object User extends User with MetaMegaProtoUser[User] {
   override def dbTableName = "users" // define the DB table name
   override def screenWrap = Full(<lift:surround with="default" at="content">
 			       <lift:bind /></lift:surround>)
-  // define the order fields will appear in forms and output
-  override def fieldOrder = List(id, firstName, lastName, email,
-  locale, timezone, password, textArea)
+  
+  //Order
+  override def fieldOrder = List(id, accountID, firstName, lastName, email,
+  locale, timezone, password, skypeID, gender, personalImageURL)
 
-  // comment this line out to require email validations
+  //Signup and edit
+  override def signupFields = List(accountID, firstName, lastName, password, email, gender, skypeID)	
+  override def editFields = List(firstName, lastName, password, gender, skypeID, personalImageURL)	
+
+
   override def skipEmailValidation = true
 }
 
-/**
- * An O-R mapped "User" class that includes first name, last name, password and we add a "Personal Essay" to it
- */
-class User extends MegaProtoUser[User] {
-  def getSingleton = User // what's the "meta" server
+class User extends MegaProtoUser[User] with ManyToMany {
+  	def getSingleton = User
+  	
+	object accountID extends MappedString(this, 30){
+		override def displayName = "Account Name"
+		override def validations = valUnique(S.?("This name is already taken")) _ :: super.validations
+	}
 
-  // define an additional field for a personal essay
-  object textArea extends MappedTextarea(this, 2048) {
-    override def textareaRows  = 10
-    override def textareaCols = 50
-    override def displayName = "Personal Essay"
-  }
-}
+	object skypeID extends MappedString(this, 30) {
+		override def displayName = "Skype ID"
+	}
+	
+	object gender extends MappedGender(this){
+		override def displayName = "Gender"
+	}
+	
+	object personalImageURL extends MappedString(this, 200){
+		override def displayName = "Personal Image"
+		override def defaultValue = "/images/img.png" 
+	}
+	
+	object coordinates extends MappedString(this, 100){
+		override def displayName = "Coordonates"
+		override def defaultValue = "NotSet" 
+	}
 
-}
+	object projects extends MappedManyToMany(UserProject, UserProject.user, UserProject.project, Project)
 }
