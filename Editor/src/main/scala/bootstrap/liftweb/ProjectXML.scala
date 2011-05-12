52cc6b2fa673dbe86e619ab195c03e71a035154d
lucaurso <lucaurso@414e8b3e-6ff4-ec89-4245-23444c5df0c2>
2011-05-12 07:48:39 +0000

    Giving a project id we can build the tree

--- Editor/src/main/scala/bootstrap/liftweb/ProjectXML.scala
+++ Editor/src/main/scala/bootstrap/liftweb/ProjectXML.scala
@@ -3,32 +3,43 @@ package bootstrap.liftweb
 import java.io.File
 import scala.xml._
 
-	
+
 class ProjectXML{
 
+
+	def projectHTML(myId: String) = {
+		val myId
+		val projBox: Box[Project] = Project.find(By(Project.id,  myId))
+		val project : Project = project.openOr(null)
+
+		val path: String = project.path.id
+		finalHTML(path)
+
+	}
+
 	def finalHTML(rootPath: String):Node = {<ul id="example" class="filetree">{dirHTML(rootPath)}</ul>}
-	
+
 	def dirHTML(path: String):Node = {
 		val list = new File(path).list
 		val fileName = path.split("/")(path.split("/").length - 1)
-		
+
 		val result = <li>
-						<span class="folder">{ fileName }</span>
-						<ul>
-							{for (aFile <- list) yield
-								{
-								val temp = new File(path +"/"+  aFile)
-								if(temp.isFile){
-									<li> <span class="file">{aFile}</span></li> 
-									
-								}else{
-									dirHTML(path +"/"+aFile)
-								}
-								}						
-							}
-						</ul>
+		<span class="folder">{ fileName }</span>
+		<ul>
+		{for (aFile <- list) yield
+			{
+			val temp = new File(path +"/"+  aFile)
+			if(temp.isFile){
+				<li> <span class="file">{aFile}</span></li> 
+
+			}else{
+				dirHTML(path +"/"+aFile)
+			}
+			}						
+		}
+		</ul>
 		</li>
-		
+
 		return result
-}
+	}
 }
