ad18b7501e4e6f0aaa60b7c6c78933dd66451e25
raph86 <raph86@414e8b3e-6ff4-ec89-4245-23444c5df0c2>
2011-05-13 12:35:00 +0000

    Removed useless code.

--- trunk/Compiler_beta/src/main/scala/code/lib/FullRest.scala
+++ trunk/Compiler_beta/src/main/scala/code/lib/FullRest.scala
@@ -14,17 +14,12 @@ import json._
 import scala.xml._
  
 /**
- * A full REST example
+ * The full REST server to compile and return the binary files
+ * and the log of the compilation.
+ *
+ * @author Raphael Schapfel
  */
 object FullRest extends RestHelper {
-  
-	// serve( "compiler" / "link" prefix {
-	// 		case Nil JsonGet _ => <item>Hello, JSON!</item>
-	// 		
-	// 		case Nil XmlGet _ => <item>Hello, XML!</item>
-	// 		
-	// 		case Nil JsonPost json -> _ => <item>{Xml.toXml(json)}</item>
-	// 	})
 
 	// Serve /compiler/link and friends
 	serve( "compiler" / "link" prefix {
@@ -72,54 +67,6 @@ object FullRest extends RestHelper {
 					// register for an "onChange" event.  When it
 					// fires, return the changed link as a response
 					Compilable.onChange(link => satisfyRequest(link: JValue))
-				}
-			}
-	})
-	
-	
-	// Serve /api/item and friends
-	serve( "api" / "item" prefix {
-     
-		// /api/item returns all the items
-		case Nil JsonGet _ => Item.inventoryItems: JValue
-
-		// /api/item/count gets the item count
-		case "count" :: Nil JsonGet _ => JInt(Item.inventoryItems.length)
- 
-		// /api/item/item_id gets the specified item (or a 404)
-		case Item(item) :: Nil JsonGet _ => item: JValue
- 
-		// /api/item/search/foo or /api/item/search?q=foo
-		case "search" :: q JsonGet _ =>
-			(for {
-				searchString <- q ::: S.params("q")
-				item <- Item.search(searchString)
-			} yield item).distinct: JValue
- 
-		// DELETE the item in question
-		case Item(item) :: Nil JsonDelete _ => 
-			Item.delete(item.id).map(a => a: JValue)
- 
-		// PUT adds the item if the JSON is parsable
-		case Nil JsonPut Item(item) -> _ => Item.add(item): JValue
-     
-		// POST if we find the item, merge the fields from the 
-		// the POST body and update the item
-		case Item(item) :: Nil JsonPost json -> _ => 
-			Item(mergeJson(item, json)).map(Item.add(_): JValue)
- 
-		// Wait for a change to the Items
-		// But do it asynchronously
-		case "change" :: Nil JsonGet _ =>
-			RestContinuation.async {
-				satisfyRequest => {
-					// schedule a "Null" return if there's no other answer
-					// after 110 seconds
-					Schedule.schedule(() => satisfyRequest(JNull), 110 seconds)
- 
-					// register for an "onChange" event.  When it
-					// fires, return the changed item as a response
-					Item.onChange(item => satisfyRequest(item: JValue))
 				}
 			}
 	})
