c3b033d1b827894f05fc55cf696f75e3de17ae16
m6r9productions@gmail.com <m6r9productions@gmail.com@414e8b3e-6ff4-ec89-4245-23444c5df0c2>
2011-05-17 06:31:10 +0000

    Fixes

--- trunk/Editor/src/main/scala/bootstrap/liftweb/Editor.scala
+++ trunk/Editor/src/main/scala/bootstrap/liftweb/Editor.scala
@@ -20,26 +20,38 @@ import code.model._
 import code.comet.ChatServer
 import scala.collection.immutable._
 import code.model.Project
+import code.comet.FileManager
 
 
 object Editor extends LiftView {
-  	def dispatch = {
-  		case filename:String => () => Full(newEditor(filename))	
-  	}
+
+	val joker :String = "_-_" 
+
+	def dispatch = {
+		case filename:String => () => Full(newEditor(filename))	
+	}
 
 	def newEditor(fileName:String): NodeSeq = {
-		
-		val s: Array[String] = fileName.trim.split(":")
-		
-		if(s.length == 2) {
-				var projectBox: Box[Project] = Project.find(By(Project.id, java.lang.Long.parseLong(s(0))))
-				var project: Project = projectBox.openOr(null)
 
-				if(project == null) {
-					errorPage()
-				} else {
-					content(fileName, project.id.is)
+		val s: Array[String] = fileName.trim.split(joker)
+
+
+		if(s.length == 2) {
+			val projectBox: Box[Project] = Project.find(By(Project.id, java.lang.Long.parseLong(s(1))))
+			val project: Project = projectBox.openOr(null)
+
+			if(project == null) {
+				errorPage()
+			} else {
+				try { 
+
+					FileManager.openFile(project.path + "/" + s(0) + ".scala")
+					content(s(0), project.id.is, true)
+
+				} catch {
+					case e: Exception => errorPage(e + "     /" + project.path + "/" + s(0) + ".scala")
 				}
+			}
 		} else if (s.length == 1) {
 			var projectBox: Box[Project] = Project.find(By(Project.id, java.lang.Long.parseLong(fileName)))
 			var project: Project = projectBox.openOr(null)
@@ -47,112 +59,125 @@ object Editor extends LiftView {
 			if(project == null) {
 				errorPage()
 			} else {
-				content(fileName, project.id.is)
+				content(fileName, project.id.is, false)
 			}
 		} else { 
-			errorPage()
+			errorPage("Non trovo un cazzo: lenght = " + s.length)
 		}
 	}
-	
+
 	def errorPage(): NodeSeq = {
 		<span>{"TODO"}</span>
 	}
-  	
-  	def content(filename: String, index: Long):NodeSeq = {	
-	
-  	 <lift:surround with="default" at="content">
+
+	def errorPage(s: String): NodeSeq = {
+		<span>{s}</span>
+	}
+
+	def content(filename: String, index: Long, file: Boolean):NodeSeq = {	
+
+		<lift:surround with="default" at="content">
 		<!--<hr>-->
 		<table id="main_table">
-			<tr>
-				<td id="left_sidebar" value="true">
-				{ProjectXML.projectHTML(index)}
-				</td>			
-				<td id="codearea">
-					<textarea id={filename} name={filename} class="editor"></textarea>
-					<div id="log">
-						<div class='logDiv'>
+		<tr>
+		<td id="left_sidebar" value="true">
+		{ProjectXML.projectHTML(index)}
+		</td>			
+		<td id="codearea">
+		<textarea id={filename + joker + index} name={filename + joker + index} class="editor">{if(file){addText(filename, index)}}</textarea>
+		<div id="log">
+		<div class='logDiv'>
 
-						<table class='logTable' cellspacing='0'>
+		<table class='logTable' cellspacing='0'>
 
-						<tr class='logTrErrorFirst'>
+		<tr class='logTrErrorFirst'>
 
-						<td class='logTd1'>17</td><td class='logTd2'><a href='http://NetBeansProjects/SA4Game/src/sa4game/Client.scala#17'>NetBeansProjects/SA4Game/src/sa4game/Client.scala:17</a></td>
+		<td class='logTd1'>17</td><td class='logTd2'><a href='http://NetBeansProjects/SA4Game/src/sa4game/Client.scala#17'>NetBeansProjects/SA4Game/src/sa4game/Client.scala:17</a></td>
 
-						</tr>
+		</tr>
 
-						<tr class='logTrErrorSecond'>
+		<tr class='logTrErrorSecond'>
 
-						<td class='logTd1'></td><td class='logTd2'>Error - not found: type HashMap</td>
+		<td class='logTd1'></td><td class='logTd2'>Error - not found: type HashMap</td>
 
-						</tr>
+		</tr>
 
-						<tr class='logTrErrorSecond'>
+		<tr class='logTrErrorSecond'>
 
-						<td class='logTd1'></td><td class='logTd2 logTdFixed'>  var games = new <u>HashMap</u>[String,Array[Array[Array[Character]]]]</td>
+		<td class='logTd1'></td><td class='logTd2 logTdFixed'>  var games = new <u>HashMap</u>[String,Array[Array[Array[Character]]]]</td>
 
-						</tr>
+		</tr>
 
-						<tr class='logTrErrorFirst'>
+		<tr class='logTrErrorFirst'>
 
-						<td class='logTd1'>12345</td><td class='logTd2'><a href='http://NetBeansProjects/SA4Game/src/sa4game/Client.scala#12345'>NetBeansProjects/SA4Game/src/sa4game/Client.scala:12345</a></td>
+		<td class='logTd1'>12345</td><td class='logTd2'><a href='http://NetBeansProjects/SA4Game/src/sa4game/Client.scala#12345'>NetBeansProjects/SA4Game/src/sa4game/Client.scala:12345</a></td>
 
-						</tr>
+		</tr>
 
-						<tr class='logTrErrorSecond'>
+		<tr class='logTrErrorSecond'>
 
-						<td class='logTd1'></td><td class='logTd2'>Error - not found: type Random</td>
+		<td class='logTd1'></td><td class='logTd2'>Error - not found: type Random</td>
 
-						</tr>
+		</tr>
 
-						<tr class='logTrErrorSecond'>
+		<tr class='logTrErrorSecond'>
 
-						<td class='logTd1'></td><td class='logTd2 logTdFixed'>    val rnd = new <u>Random</u></td>
+		<td class='logTd1'></td><td class='logTd2 logTdFixed'>    val rnd = new <u>Random</u></td>
 
-						</tr>
+		</tr>
 
-						<tr class='logTrFinalError'>
+		<tr class='logTrFinalError'>
 
-						<td class='logTd1'></td><td class='logTd2'>2 errors found</td>
+		<td class='logTd1'></td><td class='logTd2'>2 errors found</td>
 
-						</tr>
+		</tr>
 
-						<tr class='logTrFinalCompileFailure'>
+		<tr class='logTrFinalCompileFailure'>
 
-						<td class='logTd1'></td><td class='logTd2'>COMPILATION FAILED</td>
+		<td class='logTd1'></td><td class='logTd2'>COMPILATION FAILED</td>
 
-						</tr>
+		</tr>
 
-						</table>
+		</table>
 
-						</div>
-						
-					</div>
-				</td>
-					<td id="right_sidebar" value="true">
-					<div id="chat_content">
-					<lift:comet type="Chat" name={filename}>
-				        <h5 style="text-align:center">Chat</h5>
-				        <ul>
-				          <li>A message</li>
-				        </ul>
-				     </lift:comet>
-				      
-					  <div>
-				        <form class="lift:ChatIn?form=post">
-				          <input id="chat_in" />
-					      <input type="hidden" name="filename" value={filename}/>
-					      <input type="hidden" name="name" value="Jack"/>
-				          <input type="submit" value="Send"/>
-				        </form>
-				      </div>
-					</div>
-					</td>
-				</tr>
-			</table>
-			<div id="box" class="dialog">
-			<div style="text-align:center"><span id="txt">A nice file viewer</span><br />
-			<button onclick="hm('box');okSelected()">OK</button></div>
-			</div>
-      </lift:surround>
-  	}	
+		</div>
+
+		</div>
+		</td>
+		<td id="right_sidebar" value="true">
+		<div id="chat_content">
+		<lift:comet type="Chat" name={filename}>
+		<h5 style="text-align:center">Chat</h5>
+		<ul>
+		<li>A message</li>
+		</ul>
+		</lift:comet>
+
+		<div>
+		<form class="lift:ChatIn?form=post">
+		<input id="chat_in" />
+		<input type="hidden" name="filename" value={filename}/>
+		<input type="hidden" name="name" value="Jack"/>
+		<input type="submit" value="Send"/>
+		</form>
+		</div>
+		</div>
+		</td>
+		</tr>
+		</table>
+		<div id="box" class="dialog">
+		<div style="text-align:center"><span id="txt">A nice file viewer</span><br />
+		<button>OK</button></div>
+		</div>
+		</lift:surround>
+	}	
+
+	def addText(fileName: String, index: Long) : String = {
+		val projectBox: Box[Project] = Project.find(By(Project.id, index))
+		val project: Project = projectBox.openOr(null)
+
+		val s: Array[String] = fileName.trim.split(joker)
+		
+		return FileManager.openFile(project.path + "/" + s(0) + ".scala")
+	}
 }
