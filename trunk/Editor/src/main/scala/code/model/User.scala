59e6e67ff7a19a3f37cf32f5b7791e86a47c379c
mattia.candeloro89 <mattia.candeloro89@414e8b3e-6ff4-ec89-4245-23444c5df0c2>
2011-05-24 16:38:15 +0000

    modified User object

--- trunk/Editor/src/main/scala/code/model/User.scala
+++ trunk/Editor/src/main/scala/code/model/User.scala
@@ -28,64 +28,33 @@ object User extends User with MetaMegaProtoUser[User] {
   locale, timezone, password, skypeID, gender, personalImageURL)
 
   //Signup and edit
-  override def signupFields = List(accountID, firstName, lastName, password, email, gender, skypeID)
-   override def loginXhtml = <form action="/user_mgt/login" method="post">
-  <table>
-  <tr>
-  <td>Email Address</td>
-  <td><input name="username" type="text" id="F993430246327ZS4" />
-  <script type="text/javascript">
-  // <![CDATA[
-  jQuery(document).ready(function() {if (document.getElementById("F993430246327ZS4")) {document.getElementById("F993430246327ZS4").focus();};});
-  // ]]>
-  </script></td></tr>
-          <tr>
-  <td>Password</td>
-  <td><input type="password" name="password" /></td>
-  </tr>
-          <tr>
-  <td><input value="Log In" type="submit" /></td></tr>
-  </table>
-  </form>	
+  override def signupFields = List(accountID, firstName, lastName, password, email, gender, skypeID)	
   override def editFields = List(firstName, lastName, password, gender, skypeID, personalImageURL)	
-  override def signupXhtml(user: User) = <form action="/user_mgt/sign_up" method="post">
-  <table><tr>
-    <td colspan="2">Sign Up</td>
-  </tr> 
-          <tr>
-  <td>Account Name</td>
-  <td><input  name="F479282022896OO5" type="text" maxlength="30" value="" /></td>
-  </tr>
-  <tr>
-  <td>First Name</td>
-  <td><input id="txtFirstName" name="F479282022897VVR" type="text" maxlength="32" value="" /></td>
-  </tr>
-  <tr>
-  <td>Last Name</td>
-  <td><input id="txtLastName" name="F479282022898SNQ" type="text" maxlength="32" value="" /></td>
-  </tr>
-  <tr>
-  <td>Password</td>
-  <td><span><input value="*******" type="password" name="F479282022899G0X"  /> Repeat <input value="*******" type="password" name="F479282022899G0X" /></span></td>
-  </tr>
-  <tr>
-  <td>Email Address</td>
-  <td><input id="txtEmail" name="F479282022900FI4" type="text" maxlength="48" value="" /></td>
-  </tr>
-  <tr>
-  <td>Gender</td>
-  <td><select name="F479282022901Z2L"><option value="TGNKJPT1EZVOX0IW5QNO" selected="selected">Male</option><option value="4GWRCIBHZIJBWTFZQET3">Female</option></select></td>
-  </tr>
-  <tr>
-  <td>Skype ID</td>
-  <td><input  name="F479282022902WFS" type="text" maxlength="30" value="" /></td>
-  </tr> 
-          <tr>
-  <td><input name="F479282032903UCE" type="submit" value="Sign Up" /></td>
-  </tr> 
-                                </table>
-  </form>  
 
+  override def loginXhtml = <div class="logInBox"><div class="logInHome">
+  								<form action="/user_mgt/login" method="post">
+	  							<table> 
+	  								<tr><td>Email Address</td>
+	  									<td><input name="username" type="text" id="F20368288323150F" />
+	  									<script type="text/javascript"> 
+	  										// <![CDATA[
+	  										jQuery(document).ready(function() {if (document.getElementById("F20368288323150F")) {document.getElementById("F20368288323150F").focus();};});
+	  											// ]]>
+	  									</script></td></tr> 
+          							<tr><td>Password</td><td><input type="password" name="password" /></td></tr> 
+          							<tr><td><a href="/user_mgt/lost_password">Recover Password</a></td><td><input value="Log In" type="submit" /></td></tr></table> 
+  								</form>
+	  							
+	  							</div></div> 
+	 
+  override def signupXhtml(user : User) = <div class="signUpBox">
+  											<div class="SignUpHome">{ super.signupXhtml(user) }</div>
+  										 </div>  										
+  
+  
+  
+  override def changePasswordXhtml = <div id="changeBox">{ super.changePasswordXhtml }</div> 
+ 
   override def skipEmailValidation = true
 }
 
@@ -96,6 +65,7 @@ class User extends MegaProtoUser[User] with ManyToMany {
 		override def displayName = "Account Name"
 		override def validations = valUnique(S.?("This name is already taken")) _ :: super.validations
 	}
+	
 
 	object skypeID extends MappedString(this, 30) {
 		override def displayName = "Skype ID"
