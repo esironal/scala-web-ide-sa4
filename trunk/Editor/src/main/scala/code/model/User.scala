93e55f8db7e2357cd6f4d03caa20b50aca0c24f9
mattia.candeloro89 <mattia.candeloro89@414e8b3e-6ff4-ec89-4245-23444c5df0c2>
2011-05-25 13:47:17 +0000

    error messages of login, signup, edit , .. forms are visible working on css

--- trunk/Editor/src/main/scala/code/model/User.scala
+++ trunk/Editor/src/main/scala/code/model/User.scala
@@ -8,7 +8,7 @@ import net.liftweb._
 
 import util._
 import Helpers._
-
+import scala.xml._
 import common._
 import http._
 import sitemap._
@@ -28,41 +28,104 @@ object User extends User with MetaMegaProtoUser[User] {
   locale, timezone, password, skypeID, gender, personalImageURL)
 
   //Signup and edit
+//  override def loginFields = List(email, password)
   override def signupFields = List(accountID, firstName, lastName, password, email, gender, skypeID)	
   override def editFields = List(firstName, lastName, password, gender, skypeID, personalImageURL)	
 
+  
+  
+  
+  override def localForm(user: User, ignorePassword: Boolean, fields: List[FieldPointerType]): NodeSeq = {   
+    for {   
+      pointer <- fields   
+      field <- computeFieldFromPointer(user, pointer).toList   
+      if field.show_? && (!ignorePassword || !pointer.isPasswordField_?)   
+      form <- field.toForm.toList   
+    } yield { 
+     <tr><td>{field.displayName}</td>
+     	<td>{form}<lift:Msg id={field.uniqueFieldId.get} errorClass="errorMessage" />
+     </td></tr> 
+    } 
+  }   
+  
+
+  
   override def loginXhtml = <div class="logInBox"><div class="logInHome">
-  								<form action="/user_mgt/login" method="post">
-	  							<table> 
-	  								<tr><td class="fontTahoma">Email Address</td>
-	  									<td class="marginLeft"><input  name="username" type="text" id="F20368288323150F" />
-	  									<script type="text/javascript"> 
-	  										// <![CDATA[
-	  										jQuery(document).ready(function() {if (document.getElementById("F20368288323150F")) {document.getElementById("F20368288323150F").focus();};});
-	  											// ]]>
-	  									</script></td></tr> 
-          							<tr><td class="fontTahoma"> Password</td><td class="marginLeft"><input type="password" name="password" /></td></tr> 
-          							<tr><td></td><td><input id="btnLogin" value="Log In" type="submit" /></td></tr></table> 
-  								</form>
+	  						<form action="/user_mgt/login" method="post">
+	  						<table>
+	  							<tr>
+	  								<td>Email Address</td>
+	  								<td><input name="username" type="text" id="F993430246327ZS4" />
+	  									<script type="text/javascript">
+	  									//	 <![CDATA[
+	  											jQuery(document).ready(function() {if (document.getElementById("F993430246327ZS4")) {document.getElementById("F993430246327ZS4").focus();};});
+	  									//	 ]]>
+	  									</script></td></tr>
+          								<tr>
+	  										<td>Password</td>	
+	  										<td><input type="password" name="password" /></td>
+	  									</tr>
+          								<tr>
+	  										<td><input value="Log In" type="submit" /></td></tr>
+	  						</table>
+	  					</form>
 	  							
-	  							</div></div> 
-	 
-  override def signupXhtml(user : User) = <div class="signUpBox">
-  												<div class="signUpHome fontTahoma">
-  													{ super.signupXhtml(user) }
-  												</div>
-  										   </div>  										
+  							<lift:Msgs showAll="true"> 
+          					<lift:error_class>error</lift:error_class> 
+          				<lift:notice_class>notice</lift:notice_class> 
+          				<lift:warning_class>error</lift:warning_class> 
+  						</lift:Msgs> 
+  						</div>
+  						</div>
+  
+  
+  override def signupXhtml(user: User) = {
+	  
+	  <div class="signUpBox"><div class="signUpHome fontTahoma">
+	  <form method="post" action={ S.uri }> 
+	  <table>
+	  {localForm(user, false, signupFields)}
+	   <tr><td><user:submit/></td></tr>
+	  </table>	 
+	  </form>
+	   </div></div>
+	  
+  }
+
+  
    override def editXhtml(user : User) = <div class="editBox fontTahoma">
   												<div class="editPage">
-  													{ super.editXhtml(user) }
-  												</div>
-  										   </div>  	
+	  												<form method="post" action={ S.uri }> 
+  														<table>
+  															{localForm(user, false, editFields)}
+	   														<tr><td><user:submit/></td></tr>
+	  													</table>	 
+	  												</form>
+	   											</div></div>
   
   
-  override def changePasswordXhtml = <div id="changeBox"><div class="changePage fontTahoma">{ super.changePasswordXhtml }</div> </div>
-  override def lostPasswordXhtml = <div id="lostBox" class="fontTahoma"><div class="marginTop">{ super.lostPasswordXhtml }</div></div> 
+  override def changePasswordXhtml = <div id="changeBox"><div class="changePage fontTahoma">
+  										{ super.changePasswordXhtml }
+  									<lift:Msgs showAll="true"> 
+          								<lift:error_class>error</lift:error_class> 
+          								<lift:notice_class>notice</lift:notice_class> 
+          								<lift:warning_class>error</lift:warning_class> 
+  									</lift:Msgs> 
+  									</div> </div>
+  
+  
+  override def lostPasswordXhtml = <div id="lostBox" class="fontTahoma"><div class="marginTop">
+  										{ super.lostPasswordXhtml }
+  										<lift:Msgs showAll="true"> 
+          									<lift:error_class>error</lift:error_class> 
+          									<lift:notice_class>notice</lift:notice_class> 
+          									<lift:warning_class>error</lift:warning_class> 
+  										</lift:Msgs> 
+  									</div></div> 
  
   override def skipEmailValidation = true
+  
+  	
 }
 
 class User extends MegaProtoUser[User] with ManyToMany {
