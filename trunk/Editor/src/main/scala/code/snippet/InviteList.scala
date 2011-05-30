ea9dcf25f313426e88360c9b0cc9ed6012e2ff25
andrea.gallidabino@gmail.com <andrea.gallidabino@gmail.com@414e8b3e-6ff4-ec89-4245-23444c5df0c2>
2011-05-30 22:22:30 +0000

    A list which shows all the user which are not currently in the project, so that the invite is not only pure guessing

---
+++ trunk/Editor/src/main/scala/code/snippet/InviteList.scala
@@ -0,0 +1,32 @@
+package code
+package snippet
+
+
+import net.liftweb.http.LiftScreen
+import net.liftweb.http.S
+import code.model.User
+import net.liftweb.common.Full
+import code.model.Project
+import java.io.File
+import scala.xml.NodeSeq
+import net.liftweb.mapper.By
+
+object InviteList {
+
+       def whoToInvite(in: NodeSeq): NodeSeq = {
+       
+    		val projectId = S.param("id").open_!
+       		val user = User.currentUser.open_! 
+       		val listAlreadyInProject = Project.getProjectByIdAndByCurrentUser(projectId).users
+       		val allUser = User.findAll()
+       
+       		<ul>{
+                    val projectId = S.param("id").open_!.toString
+                    val users = Project.getProjectByIdAndByCurrentUser(projectId).users
+                    val possibleInvite = allUser.filter(user => !(listAlreadyInProject.contains(user)))
+                    
+                    possibleInvite.map(user => <li>{ user.accountID.is }</li>)
+        	}</ul>
+       }
+
+}
