6aa8587aaf44a036055e359c1e2ae4db64d47eb9
roccoghielmini <roccoghielmini@sunrise.ch>
2011-05-31 13:27:36 +0000

    Build Button COMPLETELY WORKING!!! Now if clicked the compilation log appears in the log tab. (ajax + rest + remote actors)

--- Editor/src/main/scala/code/snippet/BuildButton.scala
+++ Editor/src/main/scala/code/snippet/BuildButton.scala
@@ -12,76 +12,16 @@ import S._
 import util.Helpers._
 import scala.xml._
 
+object BuildButton extends StatefulSnippet {
+  private var searchString = ""
+  private var result: Box[NodeSeq] = Empty
 
-import net.liftweb.http.ListenerManager
-import net.liftweb.actor.LiftActor
-import java.io._
-import java.util.zip
-import java.net._
-import scala.io.Source
-import code.model.User
-import net.liftweb.http.S
-import code.model.Project
-import scala.collection.mutable.HashMap
-import java.io.BufferedReader
-import java.io.File
-import java.io.IOException
-import java.io.OutputStream
-import java.io.PrintStream
-import java.io.Reader
-import code.model.User._
+  val dispatch: DispatchIt = { case a:String => query(a) }
 
-object BuildButton  {
-  
-//  private var result: Box[NodeSeq] = Empty
-//
-//  val dispatch: DispatchIt = { case _ => compile }
-//  
-//  
-//  def compile = {
-//    def processEntry = {
-//      val parameters = new Array[String](1)
-//      parameters(0) = "-verbose"
-//      println("@@@@@@ ziugan prima ")
-//      S.notice(code.comet.FileManager.compile(S.param("id")+"",User.currentUser.openOr(null: User).id.is,parameters))
-//      //println("@@@@@@ ziugan dopo "+result)
-//      result = Full("ciao")
-//    }
-//
-//    "#log" #> result.openOr(<p>{SHtml.onSubmitUnit(processEntry _)}</p>)
-//  }
-  
-  
-  
-  def render = {
+  def query(log: String) = {
+      result = Full(log)
+
     
-   var result = ""
-     
-   
-     
-	   def process() = {
-     
-     val parameters = new Array[String](1)
-      parameters(0) = "-verbose"
-      println("@@@@@@ ziugan prima ")
-      result = code.comet.FileManager.compile(S.param("id")+"",User.currentUser.openOr(null: User).id.is,parameters)
-      println("@@@@@@ " + S.param("id") + " $$$$$$$$$$ " + User.currentUser.openOr(null: User).id.is )
-        
-        
-        
-   }
-     
-     
-   "type=submit" #> SHtml.onSubmitUnit(process)
-   
-   
-    
+    "#log" #> result.openOr(<p>Nothing</p>)
   }
-  
-    
-    
-    
-    
 }
-  
- 
