6c97992c5986886d4c0f5e568a68750f8aeaa03f
m6r9productions@gmail.com <m6r9productions@gmail.com@414e8b3e-6ff4-ec89-4245-23444c5df0c2>
2011-05-31 08:54:05 +0000

    Now it's possible to click on a person name to add it to a project.

--- trunk/Editor/src/main/scala/code/snippet/InviteList.scala
+++ trunk/Editor/src/main/scala/code/snippet/InviteList.scala
@@ -23,7 +23,7 @@ object InviteList {
        		<ul>{
                     val possibleInvite = allUser.filter(user => !(listAlreadyInProject.contains(user)))
                     
-                    possibleInvite.map(user => <li>{ user.accountID.is }</li>)
+                    possibleInvite.map(user => <li onclick="document.getElementById('usr').value = this.innerHTML; document.forms['addusr'].submit();">{ user.accountID.is }</li>)
         	}</ul>
        }
 
